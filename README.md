3c18a4c42e03fecd1fedb0b2318a8a3456061a94
agora <adrian.gora14@gmail.com>
2024-04-06 16:40:02 +0200

    feat: update code

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # book-ai-chat
 
-application allows you to ask questions about text that has been trained in the database using google bard model.
+application allows you to ask questions about text that has been trained in the database using openai model.
 
 ## Features
 
-- bard instead of open ai, bard is free
-- in commit history there is implemented opeai model, if you have paid version, you can use their API
+- implemented opeai model,
 - read pdf using langchain
 - postgresml to create table and train model
 
@@ -14,8 +13,12 @@ application allows you to ask questions about text that has been trained in the 
 ```bash
 npm install
 npm start
+```
 
 you need to create .env file and add value:
+
+```bash
+
 POSTGRES_URL
 POSTGRES_USER
 POSTGRES_HOST
@@ -23,14 +26,13 @@ POSTGRES_DB
 POSTGRES_PASS
 POSTGRES_PORT
 
-BARD_COOKIES
+OPENAI_API_KEY
 PORT=3000
 
 ```
 
 ### How to run
 
-- Visit https://bard.google.com/. Open the browser console by pressing F12. Go to the "Application" tab. Under "Cookies", find the cookie named **Secure-1PSID or **Secure-3PSID, find the cookie named **Secure-1PSIDCC or **Secure-3PSIDCC, find the cookie named **Secure-1PSIDTS or **Secure-3PSIDTS. Copy the value of the cookie, which will be your session value.
 - create account on https://postgresml.org/
 - connect to your PostgreSQL Database: Use the psql command to connect to your PostgreSQL database. `psql -h your_host -U your_username -d your_database`
 - create table `\i src/create.sql`
@@ -39,6 +41,5 @@ PORT=3000
 - you can read the rows by executing a SELECT query. `SELECT * FROM public.chat_pdf`
 - select pdf you want and name it `embeddings.pdf`
 - run `node embed.js` to insert loaded pdf content into table
-- craft your prompt inside `bardai.ask`
 - run `node index.js` or `npm start`
 - you can send request `POST /api/chat/completions` with body `{"input": "your question"}` from Postman or custom front end
--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 import pkg from "pg";
 const { Client } = pkg;
-import { Bard } from "googlebard";
 import cors from "cors";
 import { parseResponse } from "./helper.js";
+import OpenAI from "openai";
 
 const client = new Client({
   connectionString: process.env.POSTGRES_URL,
@@ -14,28 +14,19 @@ const client = new Client({
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASS,
-  port: Number(process.env.POSTGRES_PORT),
+  port: Number(process.env.POSTGRES_PORT)
 });
 
 client.connect();
-
 app.use(express.json(), cors());
 
-// Visit https://bard.google.com/.
-// Open the browser console by pressing F12.
-// Go to the "Application" tab.
-// Under "Cookies", find the cookie named __Secure-1PSID or __Secure-3PSID.
-// find the cookie named __Secure-1PSIDCC or __Secure-3PSIDCC.
-// find the cookie named __Secure-1PSIDTS or __Secure-3PSIDTS.
-// Copy the value of the cookie, which will be your session value.
-const GOOGLEBARD_1PSID = process.env.GOOGLEBARD_1PSID;
-const GOOGLEBARD_1PSIDCC = process.env.GOOGLEBARD_1PSIDCC;
-const GOOGLEBARD_1PSIDTS = process.env.GOOGLEBARD_1PSIDTS;
+const configuration = {
+  apiKey: process.env.OPENAI_API_KEY
+};
 
-const GOOGLEBARD_COOKIES =
-  GOOGLEBARD_1PSID + "; " + GOOGLEBARD_1PSIDCC + "; " + GOOGLEBARD_1PSIDTS;
+const GPT_MODEL = "gpt-3.5-turbo";
 
-let bardai = new Bard(GOOGLEBARD_COOKIES);
+const openai = new OpenAI(configuration);
 
 let numberOfRequests = 0;
 const PORT = process.env.PORT || 9001;
@@ -64,42 +55,47 @@ app.post(
       `;
 
       const resPkg = await client.query(query, [input]);
-
       const rows = resPkg.rows;
 
-      const completionRes = await bardai
-        .ask(
-          `
-        You are teacher who assists users with understanding a pdf. Answer the user's questions only using the context's opinion. If you are unsure of the answer, tell the user you don not know.
-
-    context's opinion: """
-    ${rows.map(({ content }: any) => content)}
-    """
-
-    user: ${input}
-
-    assistant:
-     `
-        )
-        .catch(() => {
+      const completion = await openai.chat.completions
+        .create({
+          messages: [
+            {
+              role: "system",
+              content: `You are teacher who assists users with understanding a pdf. Answer the user's questions only using the context's opinion. If you are unsure of the answer, tell the user you dont know.
+  
+              context's opinion: """
+              ${rows.map(({ content }) => content)}
+              """
+              `
+            },
+            {
+              role: "user",
+              content: input
+            }
+          ],
+          model: GPT_MODEL,
+          temperature: 0
+        })
+        .catch((e) => {
           const resp = {
             ask: input,
-            answer: "Something happened with Bard.",
+            answer: "Something happened with the AI model. Please try again."
           };
           res.send(resp);
         });
 
-      const resp = {
-        ask: input,
-        answer: parseResponse(completionRes),
-      };
-
-      res.send(resp);
+      if (completion && completion.choices) {
+        const answerToQuestion = {
+          ask: input,
+          answer: parseResponse(completion.choices[0].message.content)
+        };
+        res.send(answerToQuestion);
+      }
     } catch (err) {
-      console.log("err", err);
       res.send(err);
     }
   }
 );
 
-app.listen(PORT, () => console.log(`listening to port ${PORT}`));
+app.listen(PORT);
